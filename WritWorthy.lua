09f07d1337b33f1443673e27d040d7ea59b9762b
Sharlikran <danielhmpdx16@gmail.com>
2023-06-12 14:36:19 -0700

    Version: 7.3.7

--- WritWorthy.lua
+++ WritWorthy.lua
@@ -8,7 +8,7 @@ local WW = WritWorthy
 local LAM2 = LibAddonMenu2
 
 WritWorthy.name = "WritWorthy"
-WritWorthy.version = "7.3.6"
+WritWorthy.version = "7.3.7"
 WritWorthy.savedVarVersion = 1
 
 WritWorthy.default = {
