d8c8cea7a0166e84ba49723f822c012a37a5cee5
Sharlikran <danielhmpdx16@gmail.com>
2025-07-28 18:46:55 -0700

    Version: 7.5.1

--- WritWorthy.lua
+++ WritWorthy.lua
@@ -1,3 +1,48 @@
+--[[
+-------------------------------------------------------------------------------
+-- WritWorthy
+-------------------------------------------------------------------------------
+-- Original author: ziggr (project started 2017-02-12)
+--
+-- Current maintainer: Sharlikran (contributions since 2022-11-12)
+-- Contributor: jogietze (formerly otac0n) — GitHub PR contributions
+--
+-- ----------------------------------------------------------------------------
+-- Unless otherwise stated, portions of this software are © ziggr and may
+-- be subject to “All Rights Reserved” status due to the absence of a public
+-- open-source license declaration.
+--
+-- ----------------------------------------------------------------------------
+-- Contributions by Sharlikran are licensed under the BSD 3-Clause License:
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- 1. Redistributions of source code must retain the above copyright notice,
+--    this list of conditions and the following disclaimer.
+--
+-- 2. Redistributions in binary form must reproduce the above copyright notice,
+--    this list of conditions and the following disclaimer in the documentation
+--    and/or other materials provided with the distribution.
+--
+-- 3. Neither the name of the author "Sharlikran" nor the names of previous
+--    contributors may be used to endorse or promote products derived from this
+--    software without specific prior written permission.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED. IN NO EVENT SHALL THE
+-- COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING IN
+-- ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+-- OF SUCH DAMAGE.
+--
+-- Maintainer Notice:
+-- Redistribution of this software outside of ESOUI.com (including Bethesda.net
+-- or other platforms) is discouraged unless authorized by the current
+-- maintainer. Please do not redistribute or fork without attribution,
+-- permission, and license compliance.
+-------------------------------------------------------------------------------
+]]
 -- WritWorthy: Is this Master Writ worth doing?
 --
 -- In a master writ's tooltip, include the material cost for that writ
@@ -8,7 +53,7 @@ local WW = WritWorthy
 local LAM2 = LibAddonMenu2
 
 WritWorthy.name = "WritWorthy"
-WritWorthy.version = "7.5.0"
+WritWorthy.version = "7.5.1"
 WritWorthy.savedVarVersion = 1
 
 WritWorthy.default = {
